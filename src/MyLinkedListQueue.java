9d5b10c27af773a6f483e1565ac99166043f9905
nurali <nmakhmutzhan04@gmail.com>
2023-04-30 23:34:06 +0600

    30

--- src/MyLinkedListQueue.java
+++ src/MyLinkedListQueue.java
@@ -6,25 +6,44 @@ public class MyLinkedListQueue<T> extends MyLinkedList {
     MyLinkedListQueue() {
         myLinkedList = new MyLinkedList<>();
     }
+    /**
+     * enqueue() - method that adds an element to the back of the queue.
+     * @param element - element that you need to add.
+     */
     public void enqueue(T element) {
         myLinkedList.add(element);
     }
+    /**
+     * dequeue() - method that removes and returns the front element of the queue.
+     * @return the front element of the queue.
+     */
     public T dequeue() {
         if (isEmpty()) {
             throw new NoSuchElementException();
         }
         return (T) myLinkedList.remove(0);
     }
+    /**
+     * peek() - method that returns the front element of the queue without removing it.
+     * @return the front element of the queue without removing it.
+     */
     public T peek() {
         if (isEmpty()) {
             throw new NoSuchElementException();
         }
         return (T) myLinkedList.get(0);
     }
-
+    /**
+     * size() - method that returns the number of elements in the queue.
+     * @return the number of elements in the queue.
+     */
     public int size() {
         return myLinkedList.size();
     }
+    /**
+     * isEmpty() - method that returns true if the queue is empty or false if it is not.
+     * @return true if the queue is empty or false if it is not.
+     */
     public boolean isEmpty() {
         return myLinkedList.size() == 0;
     }
