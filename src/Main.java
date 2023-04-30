5bd7ecbb8f4405bebc38dcf143afa15d01b9bd90
nurali <nmakhmutzhan04@gmail.com>
2023-04-30 23:31:11 +0600

    28

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,32 @@ package src;
 
 public class Main {
     public static void main(String[] args) {
+        MyArrayListStack myArrayListStack = new MyArrayListStack();
+        myArrayListStack.push(1);
+        myArrayListStack.pop();
+        myArrayListStack.peek();
+        myArrayListStack.size();
+        myArrayListStack.isEmpty();
+
+        MyArrayListQueue myArrayListQueue = new MyArrayListQueue();
+        myArrayListQueue.enqueue(1);
+        myArrayListQueue.dequeue();
+        myArrayListQueue.peek();
+        myArrayListQueue.size();
+        myArrayListQueue.isEmpty();
+
+        MyLinkedListStack myLinkedListStack = new MyLinkedListStack();
+        myLinkedListStack.push(1);
+        myLinkedListStack.pop();
+        myLinkedListStack.peek();
+        myLinkedListStack.size();
+        myLinkedListStack.isEmpty();
+
+        MyLinkedListQueue myLinkedListQueue = new MyLinkedListQueue();
+        myLinkedListQueue.enqueue(1);
+        myLinkedListQueue.dequeue();
+        myLinkedListQueue.peek();
+        myLinkedListQueue.size();
+        myLinkedListQueue.isEmpty();
     }
 }
--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -12,21 +12,37 @@ public class MyLinkedListStack<T> extends MyLinkedList {
     public void push(T element) {
         myLinkedList.add(element, 0);
     }
+    /**
+     * isEmpty() - method that returns true if the stack is empty or false if it is not.
+     * @return true if the stack is empty or false if it is not.
+     */
     public boolean isEmpty() {
         return myLinkedList.size() == 0;
     }
+    /**
+     * pop() - method that removes and returns the top element of the stack.
+     * @return the top element of the stack
+     */
     public T pop() {
         if (isEmpty()) {
             throw new EmptyStackException();
         }
         return (T) myLinkedList.remove(0);
     }
+    /**
+     * peek() - method that returns the top element of the stack without removing it.
+     * @return the top element of the stack without removing it.
+     */
     public T peek() {
         if (isEmpty()) {
             throw new EmptyStackException();
         }
         return (T) myLinkedList.get(0);
     }
+    /**
+     * size() - method that returns the number of elements in the stack.
+     * @return the number of elements in the stack.
+     */
     public int size() {
         return myLinkedList.size();
     }
