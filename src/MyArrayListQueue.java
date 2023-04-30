e06b62dcc2960c6857882ace2a9c0a3bedbf3964
nurali <nmakhmutzhan04@gmail.com>
2023-04-30 22:55:57 +0600

    12

---
+++ src/MyArrayListQueue.java
@@ -0,0 +1,8 @@
+package src;
+import java.util.NoSuchElementException;
+public class MyArrayListQueue extends MyArrayList{
+    private MyArrayList myArrayList;
+    MyArrayListQueue() {
+        myArrayList = new MyArrayList();
+    }
+}
