e361c9e58b3dcb8abb30dd9b03c5bd79079ebf49
nurali <nmakhmutzhan04@gmail.com>
2023-04-30 22:47:15 +0600

    3

--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -9,6 +9,9 @@ public class MyArrayListStack extends MyArrayList {
     public void push(Object element) {
         myArrayList.add(element, 0);
     }
+    public boolean isEmpty() {
+        return myArrayList.size() == 0;
+    }
 
 
 }
