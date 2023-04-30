b4ffade8cd73ecca906f0ccd90bf7c7c9b922054
nurali <nmakhmutzhan04@gmail.com>
2023-04-30 23:25:58 +0600

    25

--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -13,18 +13,36 @@ public class MyArrayListStack extends MyArrayList {
     public void push(Object element) {
         myArrayList.add(element, 0);
     }
+    /**
+     * Returns true if the stack is empty or false if it is not.
+     * @return true if the stack is empty or false if it is not.
+     */
     public boolean isEmpty() {
         return myArrayList.size() == 0;
     }
+    /**
+     Returns the number of elements in the stack.
+     @return The number of elements in the stack.
+     */
     public int size() {
         return myArrayList.size();
     }
+    /**
+     Removes and returns the element at the top of the stack.
+     @return The element at the top of the stack.
+     @throws EmptyStackException If the stack is empty.
+     */
     public Object pop() {
         if (isEmpty()) {
             throw new EmptyStackException();
         }
         return myArrayList.remove(0);
     }
+    /**
+     Returns the element at the top of the stack without removing it.
+     @return The element at the top of the stack.
+     @throws EmptyStackException If the stack is empty.
+     */
     public Object peek() {
         if (isEmpty()) {
             throw new EmptyStackException();
