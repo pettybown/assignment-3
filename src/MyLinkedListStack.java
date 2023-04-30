1f03c0beb729e2fd877980c53c952f91ea5c3859
nurali <nmakhmutzhan04@gmail.com>
2023-04-30 22:50:34 +0600

    6

--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -1,4 +1,8 @@
 package src;
-
-public class MyLinkedListStack {
+import java.util.EmptyStackException;
+public class MyLinkedListStack<T> extends MyLinkedList {
+    private MyLinkedList<T> myLinkedList;
+    MyLinkedListStack() {
+        myLinkedList = new MyLinkedList<>();
+    }
 }
